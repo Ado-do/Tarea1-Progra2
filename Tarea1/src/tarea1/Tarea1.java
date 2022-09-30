7765b8d2091052e5f3e24dd94ee2a0cbbd804a2e
Kano10 <canomga@gmail.com>
2022-09-30 11:53:36 -0300

    Update Tarea1.java

--- Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/src/tarea1/Tarea1.java
@@ -2,7 +2,8 @@ package tarea1;
 
 public class Tarea1 {
     public static void main(String[] args) {
-        
+        System.out.println("sexo");
     }    
+    
 }
 //chupala adodo y te odio cano
