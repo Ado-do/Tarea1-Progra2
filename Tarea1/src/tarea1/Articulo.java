7a489d66878c95e8d55bd944ee4739a519c6bee5
Alonso Bustos <alonbustos2021@udec.cl>
2022-09-30 13:07:09 -0300

    Update Articulo

--- Tarea1/src/tarea1/Articulo.java
+++ Tarea1/src/tarea1/Articulo.java
@@ -1,12 +1,27 @@
 package tarea1;
 
 public class Articulo {
-    private float peso;
     private String nombre;
+    private float peso;
     private String descripcion;
     private float precio;
 
-    public Articulo() {
-        
+    public Articulo(String nombre, float peso, String descripcion, float precio) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getDescription() {
+        return descripcion;
+    }
+    public float getPrecio() {
+        return precio;
     }
 }
