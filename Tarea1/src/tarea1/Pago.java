af1f0bbb3baad7819428621bf88e972728f8f5ee
Kano10 <canomga@gmail.com>
2022-10-08 18:34:12 -0300

    pago
    creo que está listo lo de pago xd

--- Tarea1/src/tarea1/Pago.java
+++ Tarea1/src/tarea1/Pago.java
@@ -3,22 +3,27 @@ package tarea1;
 import java.util.Date;
 
 public abstract class Pago {
-    private float monto;
     private Date fecha;
-    
+    public DetalleOrden detalleOrden;
+    private float monto = detalleOrden.getcalcPrecio();
     public Pago() {
         
     }
 }
 
 class Efectivo extends Pago {
-
+    float pago;
+    public float calcDevolucion(float pago, float monto){
+        return pago - monto; 
+    }
 }
 
 class Transferencia extends Pago {
-
+    private String banco;
+    private String numCuenta;
 }
 
 class Tarjeta extends Pago {
-       
+       private String tipo;
+       private String numTransacción;
 }
