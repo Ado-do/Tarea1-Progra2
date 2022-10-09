f7741ad1b987fe25a5091ab4972eb31585c82303
Kano10 <canomga@gmail.com>
2022-10-09 17:52:33 -0300

    Update Pago.java

--- Tarea1/src/tarea1/Pago.java
+++ Tarea1/src/tarea1/Pago.java
@@ -3,27 +3,93 @@ package tarea1;
 import java.util.Date;
 
 public abstract class Pago {
-    private Date fecha;
-    public OrdenCompra OrdenCompra;
-    private float monto = OrdenCompra.calcPrecio();
-    public Pago() {
-        
+    protected Date fecha;
+    protected float monto;
+    public Pago(float monto, Date fecha) {
+        this.monto = monto;
+        this.fecha = (fecha == null) ? new Date() : fecha;
+    }
+    public float getMonto() {
+        return monto;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    @Override
+    public String toString() {
+        return " Monto: $"+monto+"\n Fecha: "+fecha;
     }
 }
 
 class Efectivo extends Pago {
-    float pago;
-    public float calcDevolucion(float pago, float monto){
-        return pago - monto; 
+    public Efectivo(float monto, Date fecha){
+        super(monto, fecha);
+    }
+    public float calcDevolucion(float montoTotal){
+        return (this.monto > montoTotal) ? (this.monto -montoTotal) : 0; 
+    }
+    @Override
+    public String toString() {
+        return "Pago con efectivo:\n"+super.toString();
     }
 }
 
 class Transferencia extends Pago {
     private String banco;
     private String numCuenta;
+    public Transferencia(float monto, Date fecha, String banco, String numCuenta){
+        super(monto, fecha);
+        this.banco = banco;
+        this.numCuenta = numCuenta;
+    }
+    public String getBanco() {
+        return banco;
+    }
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+    @Override
+    public String toString() {
+        return "Pago por transferencia:\n"+super.toString()+"\n Banco: "+banco+"\n Número de cuenta: "+numCuenta;
+    }
 }
 
 class Tarjeta extends Pago {
-       private String tipo;
-       private String numTransacción;
+    private String tipo;
+    private String numTransacción;
+    private String numCuenta;
+    public Tarjeta(float monto, Date fecha, String tipo, String numCuenta){
+        super(monto, fecha);
+        this.tipo = tipo;
+        this.numCuenta = numCuenta;
+        
+    }   
+    public String getTipo() {
+        return tipo;
+    }
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+    @Override
+    public String toString() {
+        return "Pago con tarjeta:\n"+super.toString()+"\n Tipo de tarjeta: "+tipo+"\n Número de cuenta: "+numCuenta;
+    }
 }
